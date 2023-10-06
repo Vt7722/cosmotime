e05d4b0b8213c38685fcafc0d4c30b489ac10544
Вероника Тумакова <82195822+Vt7722@users.noreply.github.com>
2023-10-06 16:05:56 +0600

    Update main.js

--- main.js
+++ main.js
@@ -222,21 +222,21 @@ function send(data) {
 
   data += '\n';
 
-  if (data.length > 20) {
-    let chunks = data.match(/(.|[\r\n]){1,20}/g);
+  // if (data.length > 20) {
+  //   let chunks = data.match(/(.|[\r\n]){1,20}/g);
 
-    writeToCharacteristic(characteristicCache, chunks[0]);
+  //   writeToCharacteristic(characteristicCache, chunks[0]);
 
-    for (let i = 1; i < chunks.length; i++) {
-      setTimeout(() => {
-        writeToCharacteristic(characteristicCache, chunks[i]);
-      }, i * 100);
-    }
-  }
-  else {
-    writeToCharacteristic(characteristicCache, data);
-  }
-
+  //   for (let i = 1; i < chunks.length; i++) {
+  //     setTimeout(() => {
+  //       writeToCharacteristic(characteristicCache, chunks[i]);
+  //     }, i * 100);
+  //   }
+  // }
+  // else {
+  //   writeToCharacteristic(characteristicCache, data);
+  // }
+  writeToCharacteristic(characteristicCache, data);
   log(data, 'out');
 }
 
